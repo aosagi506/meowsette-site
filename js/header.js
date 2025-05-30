f90b1581f1046d7e5f8409a48133e05088b5f237
aosagi506 <gripen.sky.fighter@gmail.com>
2025-05-30 22:59:20 +0900

    Update:修正

--- js/header.js
+++ js/header.js
@@ -5,16 +5,14 @@ fetch('header.html')
         const header = document.getElementById('header');
         header.innerHTML = data;
         header.classList.add("loaded");
+        const overlay = document.getElementById('loadingOverlay');
+        if (overlay) {
+            setTimeout(() => {
+                overlay.classList.add('loaded');
+            }, 100);
+        }
     })
     .catch(err => console.error('読み込みエラー', err));
-document.addEventListener("DOMContentLoaded", () => {
-    const overlay = document.getElementById('loadingOverlay');
-    if (overlay) {
-        setTimeout(() => {
-            overlay.classList.add('loaded');
-        }, 100);
-    }
-});
 function spMenu() {
     document.getElementById('spMenuHidden').style.display = 'none';
     document.getElementById('spMenuVisible').style.display = 'block';
