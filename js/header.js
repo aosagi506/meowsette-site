6eaa85d4790a0414e5066544bc73881962157217
aosagi506 <gripen.sky.fighter@gmail.com>
2025-05-30 22:49:37 +0900

    Update:細部の更新

--- js/header.js
+++ js/header.js
@@ -6,4 +6,26 @@ fetch('header.html')
         header.innerHTML = data;
         header.classList.add("loaded");
     })
-    .catch(err => console.error('読み込みエラー',err));
+    .catch(err => console.error('読み込みエラー', err));
+document.addEventListener("DOMContentLoaded", () => {
+    const overlay = document.getElementById('loadingOverlay');
+    if (overlay) {
+        setTimeout(() => {
+            overlay.classList.add('loaded');
+        }, 100);
+    }
+});
+function spMenu() {
+    document.getElementById('spMenuHidden').style.display = 'none';
+    document.getElementById('spMenuVisible').style.display = 'block';
+}
+function spMenu2() {
+    document.getElementById('spMenuHidden').style.display = 'block';
+    document.getElementById('spMenuVisible').style.display = 'none';
+}
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1024) {
+        document.getElementById('spMenuHidden').style.display = '';
+        document.getElementById('spMenuVisible').style.display = '';
+    }
+})
